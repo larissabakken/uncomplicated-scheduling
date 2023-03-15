284ae5c997c447b51d15de263c2b5d50017efdfb
Larissa Ramos <devlarissasr@gmail.com>
2023-03-15 12:59:18 -0300

    bringing gitlab repository

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,42 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo'
+
+export default function Home() {
+  return (
+    <>
+      <NextSeo
+        title="Uncomplicated Scheduling"
+        description="Connect your calendar and let people book appointments in their free time"
+      />
+
+      <Container>
+        <Hero>
+          <Heading as="h1" size="4xl">
+            Agendamento descomplicado
+          </Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
+
+          <ClaimUsernameForm />
+        </Hero>
+
+        <Preview>
+          <Image
+            src={previewImage}
+            height={400}
+            quality={100}
+            priority
+            alt="Calendar symbolizing application in operation"
+          />
+        </Preview>
+      </Container>
+    </>
+  )
+}
